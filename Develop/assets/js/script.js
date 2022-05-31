9e65da8583e93c6511a382a9b2e1155389239138
Ommar Khawaja <ommarkhawaja1@gmail.com>
2022-05-30 22:58:15 -0400

    added rows in html

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,5 @@
+// display current day at the top of the calendar
+// each time block is color coded based off past, current, future
+// when I click into a time block then I can enter an event
+// when I click the save button for that time block then the text for that event is saved in local storage
+// when I refresh the page then the saved events persist
