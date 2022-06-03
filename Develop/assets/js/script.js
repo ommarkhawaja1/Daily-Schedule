fb362cc49d789bf43db6162c6babcde14b7d5ca1
Ommar Khawaja <ommarkhawaja1@gmail.com>
2022-06-03 17:11:38 -0400

    html setup

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -3,3 +3,11 @@
 // when I click into a time block then I can enter an event
 // when I click the save button for that time block then the text for that event is saved in local storage
 // when I refresh the page then the saved events persist
+
+// when save button is clicked, then store text in localstorage associated with specific time 
+// message appears at the top when saving (appointment added to local storage)
+// get value of text and value of time and save it in localstorage
+
+
+
+
